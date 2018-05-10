9f645d0fe4651af7d1a31c19a3664556b6a170f1
Biel Pawel <pako-96@o2.pl>
2018-05-10 08:03:17 +0200

    More pep8

--- config.py
+++ config.py
@@ -12,5 +12,6 @@ class DevConfig(Config):
     DATABASE_URI = getenv('DEV_DATABASE_URI', '')
     SQLALCHEMY_DATABASE_URI = 'postgresql://user:pass@127.0.0.1:5532/name'
 
+
 class ProdConfig(Config):
     DATABASE_URI = getenv('PROD_DATABASE_URI', '')
