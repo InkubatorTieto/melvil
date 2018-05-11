42813ae0833a09043de04431a67aa24cb02e9d4e
Mamrot Izabela <izabela.mamrot@tieto.com>
2018-05-11 09:55:29 +0200

    fix problem in config and docker-compose

--- config.py
+++ config.py
@@ -2,13 +2,21 @@ from os import getenv
 
 
 class Config(object):
-    DEBUG = False
-    TESTING = False
+   DEBUG = False
+   TESTING = False
 
 class DevConfig(Config):
-    DEBUG = True
-    DATABASE_URI = getenv('DEV_DATABASE_URI', '')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://test:test@localhost:5432/test_db'
+   DEBUG = True
+   DATABASE_URI = getenv('DEV_DATABASE_URI', '')
+   DB_ENGINE = getenv('DB_ENGINE')
+   DB_USER = getenv('DB_USER')
+   DB_PASSWORD = getenv('DB_PASSWORD')
+   DB_HOST = getenv('DB_HOST')
+   DB_PORT = getenv('DB_PORT')
+   DB_NAME = getenv('DB_NAME')
+
+   SQLALCHEMY_DATABASE_URI = '{0}://{1}:{2}@{3}:{4}/{5}'.format(DB_ENGINE, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+
 
 class ProdConfig(Config):
-    DATABASE_URI = getenv('PROD_DATABASE_URI', '')
+   DATABASE_URI = getenv('PROD_DATABASE_URI', '')
