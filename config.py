bea1109f1c2339b1e6d61169b66bd4237a0e6cc5
Mamrot Izabela <izabela.mamrot@tieto.com>
2018-05-11 10:09:18 +0200

    fix problem in config v2

--- config.py
+++ config.py
@@ -2,28 +2,29 @@ from os import getenv
 
 
 class Config(object):
-   DEBUG = False
-   TESTING = False
-   DATABASE_URI = ''
+    DEBUG = False
+    TESTING = False
+    DATABASE_URI = ''
+
 
 class DevConfig(Config):
-   DEBUG = True
-   DB_ENGINE = getenv('DB_ENGINE')
-   DB_USER = getenv('DB_USER')
-   DB_PASSWORD = getenv('DB_PASSWORD')
-   DB_HOST = getenv('DB_HOST')
-   DB_PORT = getenv('DB_PORT')
-   DB_NAME = getenv('DB_NAME')
+    DEBUG = True
+    DB_ENGINE = getenv('DB_ENGINE')
+    DB_USER = getenv('DB_USER')
+    DB_PASSWORD = getenv('DB_PASSWORD')
+    DB_HOST = getenv('DB_HOST')
+    DB_PORT = getenv('DB_PORT')
+    DB_NAME = getenv('DB_NAME')
 
-   SQLALCHEMY_DATABASE_URI = '{0}://{1}:{2}@{3}:{4}/{5}'.format(
+    SQLALCHEMY_DATABASE_URI = '{0}://{1}:{2}@{3}:{4}/{5}'.format(
        DB_ENGINE,
        DB_USER,
        DB_PASSWORD,
        DB_HOST,
        DB_PORT,
        DB_NAME,
-   )
+    )
 
 
 class ProdConfig(Config):
-   DATABASE_URI = getenv('PROD_DATABASE_URI', '')
+    DATABASE_URI = getenv('PROD_DATABASE_URI', '')
