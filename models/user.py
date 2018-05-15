7e1f59f70f565e8cb909a257d8a0a7658af487a2
oskam <oska.magdalena@gmail.com>
2018-05-15 14:09:56 +0200

    basic user and role models

---
+++ models/user.py
@@ -0,0 +1,28 @@
+from app import db
+from flask_user import UserMixin
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), unique=True)
+    first_name = db.Column(db.String(64))
+    surname = db.Column(db.String(64))
+    password_hash = db.deferred(db.Column(db.String(128)))
+    active = db.Column(db.Boolean)
+    roles = db.relationship('Role', secondary='user_roles')
+
+
+class UserRoles(db.Model):
+    __tablename__ = 'user_roles'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id', ondelete='CASCADE'))
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+
