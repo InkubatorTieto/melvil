62750fa0df50c0670edf91ce4bea6f47e8a5e062
Ochowicz Marta <marta.ochowicz@tieto.com>
2018-05-15 14:20:02 +0200

    initial commit

---
+++ alembic/versions/19e1c82fa769_users_table_created.py
@@ -0,0 +1,24 @@
+"""users table created
+
+Revision ID: 19e1c82fa769
+Revises: 63f3099ace00
+Create Date: 2018-05-11 10:52:36.024717
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '19e1c82fa769'
+down_revision = '63f3099ace00'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- views/index.py
+++ views/index.py
@@ -5,3 +5,17 @@ from . import library
 @library.route('/')
 def index():
     return render_template('index.html')
+
+@library.route('/login')
+def login():
+    return render_template('login.html')
+
+@library.route('/register')
+def register():
+    return render_template('register.html')
+
+@library.route('/search')
+def search():
+    return render_template('browse.html')
+
+
