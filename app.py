8966ef808975f1f78178d574723fee7fa967e566
Biel Pawel <pako-96@o2.pl>
2018-05-16 13:35:33 +0200

    Complete

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from flask import Flask
-from views import library
-from config import DevConfig
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
+from flask import Flask
+from config import DevConfig
+from flask_login import LoginManager
+from views import library
+import os
+
+login_manager = LoginManager()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(DevConfig)
     app.register_blueprint(library)
-
+    app.secret_key = os.urandom(24)
     db.init_app(app)
+    login_manager.init_app(app)
 
     return app
--- views/index.py
+++ views/index.py
@@ -1,7 +1,32 @@
-from flask import render_template
+from flask import render_template, request, session
 from . import library
+from models.user import User
 
 
 @library.route('/')
 def index():
     return render_template('index.html')
+
+
+@library.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        print("To ja")
+        email = request.form['email']
+        passw = request.form['password']
+        try:
+            data = User.query.filter_by(email=email, password_hash=passw).first()
+            print(data)
+
+            if data is not None:
+                print(data)
+                session['logged_in'] = True
+                print("Sesja się udała")
+                return render_template('index.html')
+
+            else:
+                return 'Login failed'
+        except:
+            return "Login failed"
