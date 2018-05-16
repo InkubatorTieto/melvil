514efb0f072e1f3f2dde45c673f7182d2d14dbe9
Panineyeva Lizaveta <lizaveta.panineyeva@tieto.com>
2018-05-16 14:23:19 +0200

    fixed send_email functionality, test for send_email and template for registration email

--- tests/test_send_mail.py
+++ tests/test_send_mail.py
@@ -1,12 +1,12 @@
 from send_email import send_email
 from app import mail
-import pytest
+from config import DevConfig
 
 
 def test_send(app):
 
     with mail.record_messages() as outbox:
-        send_email('testing', 'tieto.library@gmail.com', ['tieto.library@gmail.com'], 'test', 'test')
+        send_email('testing', DevConfig.ADMINS[0], ['tieto.library@gmail.com'], 'test', 'test')
         assert len(outbox) == 1
         msg = outbox[0]
         assert msg.subject == "testing"
--- views/index.py
+++ views/index.py
@@ -5,10 +5,4 @@ from send_email.emails import *
 
 @library.route('/')
 def index():
-    send_email('Tieto-Library',
-               'tieto.library@gmail.com',
-               ['tieto.library@gmail.com'],
-               '',
-               render_template("registration_email.html"))
-
     return render_template('index.html')
