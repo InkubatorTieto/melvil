047719a23dd6c54451e93a34e8f58e51808a4140
Biel Pawel <pako-96@o2.pl>
2018-05-18 14:50:14 +0200

    fixes

--- forms/login_form.py
+++ forms/login_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email')
