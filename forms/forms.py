a2db2f3e189b252ba4d0419479e3fbeb5b6e6b92
Biel Pawel <pako-96@o2.pl>
2018-05-23 09:41:03 +0200

    Stickler fixes

--- forms/forms.py
+++ forms/forms.py
@@ -4,32 +4,51 @@ from wtforms.validators import DataRequired, Email, EqualTo
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()], render_kw=({'class': 'inputs', 'placeholder': 'Email'}))
-    password = PasswordField('Password', validators=[DataRequired()], render_kw=({'class': 'inputs', 'placeholder': 'Password'}))
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()],
+                        render_kw=({'class': 'inputs', 'placeholder': 'Email'}))
+    password = PasswordField('Password',
+                             validators=[DataRequired()],
+                             render_kw=({'class': 'inputs', 'placeholder': 'Password'}))
     remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In', render_kw=({'class': 'btn btn-primary submits'}))
+    submit = SubmitField('Sign In',
+                         render_kw=({'class': 'btn btn-primary submits'}))
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Email', validators=[Email()], render_kw=({'class': 'inputs', 'placeholder': 'Email'}))
-    first_name = StringField('First name', validators=[DataRequired()], render_kw=({'class': 'inputs',
-                                                                                    'placeholder': 'First Name'}))
-    surname = StringField('Surname', validators=[DataRequired()], render_kw=({'class': 'inputs',
-                                                                              'placeholder': 'Surname'}))
-    password = PasswordField('Password', validators=[DataRequired(),
-                                                     EqualTo('confirm_pass', message='Passwords must match.')],
+    email = StringField('Email',
+                        validators=[Email()],
+                        render_kw=({'class': 'inputs', 'placeholder': 'Email'}))
+    first_name = StringField('First name',
+                             validators=[DataRequired()],
+                             render_kw=({'class': 'inputs', 'placeholder': 'First Name'}))
+    surname = StringField('Surname',
+                          validators=[DataRequired()],
+                          render_kw=({'class': 'inputs', 'placeholder': 'Surname'}))
+    password = PasswordField('Password',
+                             validators=[DataRequired(), EqualTo('confirm_pass', message='Passwords must match.')],
                              render_kw=({'class': 'inputs', 'placeholder': 'Password'}))
-    confirm_pass = PasswordField('Confirm password', validators=[DataRequired()],
-                                render_kw=({'class': 'inputs', 'placeholder': 'Confirm Password'}))
-    submit = SubmitField('Sign In', render_kw=({'class': 'btn btn-primary submits'}))
+    confirm_pass = PasswordField('Confirm password',
+                                 validators=[DataRequired()],
+                                 render_kw=({'class': 'inputs', 'placeholder': 'Confirm Password'}))
+    submit = SubmitField('Sign In',
+                         render_kw=({'class': 'btn btn-primary submits'}))
 
 
 class ContactForm(FlaskForm):
-    email = StringField('email', validators=[Email()], render_kw=({'class': 'inputs', 'placeholder': 'Email'}))
-    title = StringField('title', validators=[DataRequired()], render_kw=({'class': 'inputs', 'placeholder': 'Title'}))
-    message = TextAreaField('message', validators=[DataRequired()], render_kw=({'class': 'inputs message',
-                                                                                'placeholder': 'Message'}))
-    send_message = SubmitField('Send message', render_kw=({'class': 'btn btn-primary submits'}))
+    email = StringField('email',
+                        validators=[Email()],
+                        render_kw=({'class': 'inputs', 'placeholder': 'Email'}))
+    title = StringField('title',
+                        validators=[DataRequired()],
+                        render_kw=({'class': 'inputs', 'placeholder': 'Title'}))
+    message = TextAreaField('message',
+                            validators=[DataRequired()],
+                            render_kw=({'class': 'inputs message', 'placeholder': 'Message'}))
+    send_message = SubmitField('Send message',
+                               render_kw=({'class': 'btn btn-primary submits'}))
+
+
 class SearchForm(FlaskForm):
     query = StringField('Search')
     submit = SubmitField('Search')
