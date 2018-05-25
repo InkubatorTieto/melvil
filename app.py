c4b121604b918190f48544c9d070cda4b2a38bbb
Magdalena Sliwinska <magdalena.sliwinska@tieto.com>
2018-05-25 10:35:47 +0200

    xlsx reader improvement, adding books to the database enabled

--- app.py
+++ app.py
@@ -11,8 +11,9 @@ from raven import Client
 mail = Mail()
 sentry = Sentry()
 client = Client()
-
-
+from flask_sqlalchemy import SQLAlchemy
+from xlsx_reader import get_book_data
+db = SQLAlchemy()
 
 def create_app(config=DevConfig):
 
@@ -24,11 +25,55 @@ def create_app(config=DevConfig):
     mail.init_app(app)
 
     db.init_app(app)
+
+    from models import (Book, Author)
     with app.app_context():
         db.create_all()
 
-    ##
-    from xlsx_reader import insertDb
-    insertDb()
-    ##
+        # test book object
+        book1 = Book()
+        book1.title = 'SPRZEDAM OPLA'
+        db.session.add(book1)
+
+#add tile
+        books_properties = get_book_data()
+        book = Book()
+        authors = Author()
+
+        for elem in books_properties:
+            title = elem['title']
+        #    asset = elem['asset']
+        #    title = book['title']
+            book = Book(title = title)
+        #    book.title = title
+
+            authors = elem['authors']
+            #print(type(authors))
+
+            #under construction:
+            # TODO: HANDLE WITH DIFFERENT TYPES OF AUTHORS
+            '''for i in authors:
+                type_i = type(i)
+                if type_i is str:
+                    print(i, 'string!')
+                elif type_i is tuple:
+                    f_name = i[0]
+                    l_name = i[1]
+                    author = Author(first_name = f_name, last_name = l_name)
+                    print(i, 'tuple!', author)
+                    db.session.add(author)
+                else: # for list of authors
+                    for elem in i:
+                        print(i, 'list!')'''
+                #print(f_name, l_name)
+                    #db.session.add(author)
+                #else:
+                #    pass
+            db.session.add(book)
+        db.session.commit()
+        print(Book.query.all()) #shows elements added to the database
+
     return app
+
+create_app(DevConfig)
+
--- xlsx_reader.py
+++ xlsx_reader.py
@@ -1,44 +1,73 @@
 import xlrd
 from nameparser import HumanName
 
-
-data = './biblioteka.xlsx'
+data = './data/biblioteka.xlsx'
 workbook = xlrd.open_workbook(data)
 
 
-for sheet_index in range(workbook.nsheets-2):   # -2 excludes and deleted magazines sheet  #TODO: def func. properly
-    current_sheet = workbook.sheet_by_index(sheet_index)
-    # TODO: add name of the sheet to db
-    rows = current_sheet.nrows
-    cols = current_sheet.ncols
-    for row_index in range(0, rows):
-        current_row = current_sheet.row(row_index)
-        title = current_sheet.cell_value(row_index, 1)
-        asset = current_sheet.cell_value(row_index, 3)
-        authors = (current_sheet.cell_value(row_index, 2))
-        # TODO: add information about user, date of rental, status
-
-        if (',' in authors and 'Jr.' not in authors) or (' and ' in authors) or ('&' in authors):
-            splitted_authors = authors.replace(' and ', ',').replace('&', ',').split(',')
-            splitted = []
-            for auth in splitted_authors:
-                splitted.append(auth)
-                # TODO: remember to add all authors when multiple
-                name = HumanName(str(auth))
-                first_name = name.first
-                last_name = name.last
-                # TODO: add author to db
-
-        else:
-            name = HumanName(authors)
+def get_author_name(authors):
+    if (',' in authors and 'Jr.' not in authors) or (' and ' in authors) or ('&' in authors):
+        splited_authors = authors.replace(' and ', ',').replace('&', ',').split(',')
+        ath = []
+        first_names = []
+        last_names = []
+        for auth in splited_authors:
+            name = HumanName(str(auth))
             first_name = name.first
             last_name = name.last
-            # TODO: add author to db
-        #print(title, authors, 'FIRST NAME: ', first_name, 'LAST NAME: ', last_name)
+            first_names.append(first_name)
+            last_names.append(last_name)
+            ath = list(zip(first_names, last_names))
+    else:
+        name = HumanName(str(authors))
+        first_name = name.first
+        last_name = name.last
+        ath = first_name, last_name
+    return ath
 
+def get_book_data():
+    book_list = []
+    book_dict = {}
+    for sheet_index in range(workbook.nsheets-1):   # -2 excludes and deleted magazines sheet
+        current_sheet = workbook.sheet_by_index(sheet_index)
+        rows = current_sheet.nrows
+
+        for row_index in range(1, rows): # there is no need to load title of the column
+            current_shelf = current_sheet.name #TODO: which place in db? change the name of the property? ask Magda O.
+            title = current_sheet.cell_value(row_index, 1)
+            authors = current_sheet.cell_value(row_index, 2)
+            asset = current_sheet.cell_value(row_index, 3)
+            author = get_author_name(authors)
+
+            if current_shelf == 'General':
+                user = current_sheet.cell_value(row_index, 4)
+                date_of_rental = current_sheet.cell(row_index, 5)
+                status = current_sheet.cell(row_index, 5)
+                author = get_author_name(authors)
+                book_properties = {'authors': author, 'title': title, 'asset': asset, 'user': user}
+                book_list.append(book_properties)
+
+                book_dict.update({'authors': author, 'title': title, 'asset': asset, 'user': user})
+            elif current_shelf == 'Magazines':
+                author = ' '
+                title = ' '
+                edition = current_sheet.cell_value(row_index, 2)
+                number = current_sheet.cell_value(row_index, 3) # ISSUE
+                magazine_properties = {'authors': author, 'title': title, 'asset': asset, 'user': user, 'edition': edition, 'number': number}
+                book_dict.update({'authors': author, 'title': title, 'asset': asset, 'user': user, 'edition': edition, 'number': number})
+                book_list.append(magazine_properties)
+            else:
+                pass #TODO: ensure if there is any exception to cover in Managers section?
+                #book ={'authors': author, 'current_shelf': current_shelf, 'title': title, 'asset': asset, 'user': user,
+                    #'date_of_rental': date_of_rental, 'status': status}
+                book_properties = {'authors': author, 'current_shelf': current_shelf, 'title': title, 'asset': asset, 'user': user,
+                    'date_of_rental': date_of_rental, 'status': status}
+                book_list.append(book_properties)
+   # print(book_list)
+    return book_list
 
 # TODO: find inapropriate names -->  customize parser configuration
-# TODO: find the solution for magazines
-# TODO: connect with DB
-# TODO: push parameters into db
+# TODO: find the solution for magazines - checked
+# TODO: connect with DB - checked
+# TODO: push parameters into db - under construction
 # TODO: def func in order not to repeat code
