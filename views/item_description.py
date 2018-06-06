e64a413ca3512db6fa3a80ba16244311f22badbc
oskam <oska.magdalena@gmail.com>
2018-06-06 09:59:54 +0200

    paddings and margins styles, error handlers

--- views/item_description.py
+++ views/item_description.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, session
+from flask import render_template, Blueprint, session, abort
 from models import LibraryItem, User
 
 item_desc = Blueprint('item_description', __name__,
@@ -7,8 +7,13 @@ item_desc = Blueprint('item_description', __name__,
 
 @item_desc.route('/item_description/<int:item_id>')
 def item_description(item_id):
-    user = User.query.get(session['id'])
-    admin = user.has_role('ADMIN')
+    try:
+        user = User.query.get(session['id'])
+        admin = user.has_role('ADMIN')
+    except KeyError:
+        abort(401)
+    except Exception:
+        abort(500)
     item = LibraryItem.query.get_or_404(item_id)
     tags_list = item.tags_string()
 
@@ -25,8 +30,26 @@ def item_description(item_id):
 
 @item_desc.errorhandler(404)
 def not_found(error):
-    message_body = 'Item does not exist!'
+    message_body = 'Page does not exist!'
     message_title = 'Error!'
     return render_template('message.html',
                            message_title=message_title,
                            message_body=message_body), 404
+
+
+@item_desc.errorhandler(401)
+def not_authorized(error):
+    message_body = 'You are not authorized to visit this site!'
+    message_title = 'Error!'
+    return render_template('message.html',
+                           message_title=message_title,
+                           message_body=message_body), 401
+
+
+@item_desc.errorhandler(500)
+def server_error(error):
+    message_body = 'but something went wrong.'
+    message_title = 'Don\'t panic!'
+    return render_template('message.html',
+                           message_title=message_title,
+                           message_body=message_body), 500
