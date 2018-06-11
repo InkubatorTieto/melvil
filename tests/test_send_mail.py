5cd628950487161a5aa644febb5b331953c65afd
jahenstein <lu.jaszewski@gmail.com>
2018-06-11 12:25:44 +0200

    WIP experiment with email test, sorry for mess

--- tests/test_send_mail.py
+++ tests/test_send_mail.py
@@ -1,14 +1,14 @@
-# import smtplib
-# import email.utils
-# from email.mime.text import MIMEText
-#
-#
-# msg = MIMEText('The body of your message.')
-# msg['To'] = email.utils.formataddr(('Recipient Name',
-#                                     'jahenstein@gmail.com'))
-# msg['From'] = email.utils.formataddr(('Your Name', 'test@test.com'))
-# msg['Subject'] = 'Your Subject'
-#
-# server = smtplib.SMTP()
-# server.connect()
-# server.sendmail('test@test.com', ['jahenstein@gmail.com'], msg.as_string())
+import smtplib
+import email.utils
+from email.mime.text import MIMEText
+
+
+msg = MIMEText('The body of your message.')
+msg['To'] = email.utils.formataddr(('Recipient Name',
+                                    'jahenstein@gmail.com'))
+msg['From'] = email.utils.formataddr(('Your Name', 'test@test.com'))
+msg['Subject'] = 'Your Subject'
+
+server = smtplib.SMTP()
+server.connect()
+server.sendmail('test@test.com', ['jahenstein@gmail.com'], msg.as_string())
