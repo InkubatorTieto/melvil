10881cb2a9eb8cb96b6bcd558dc7252bee7484e9
jahenstein <lu.jaszewski@gmail.com>
2018-06-13 09:49:12 +0200

    test for decorators - working!

--- tests/test_decorators_roles.py
+++ tests/test_decorators_roles.py
@@ -55,14 +55,10 @@ def test_login_decorators_custom_redirect(login, redirect_page):
         return 'ok'
 
     if login:
-        if func_logged_in() == 'ok' and func_not_logged_in() != 'ok':
-            assert True
-        else:
+        if func_logged_in() != 'ok' and func_not_logged_in() == 'ok':
             assert False
     else:
-        if func_logged_in() != 'ok' and func_not_logged_in() == 'ok':
-            assert True
-        else:
+        if func_logged_in() == 'ok' and func_not_logged_in() != 'ok':
             assert False
 
 
@@ -75,19 +71,22 @@ def test_role_decorator(role, required_role):
         first_name=g.person.name(),
         surname=g.person.surname(),
         password_hash=g.cryptographic.hash(),
-        active=g.development.boolean(),
+        active=g.development.boolean()
     )
     db.session.add(new_user)
     db.session.commit()
 
     data = User.query.filter_by(email=email).first()
-    data.roles[0] = Role(name=role)
-
-    db.session.commit()
 
     session['logged_in'] = True
     session['id'] = data.id
     session['email'] = data.email
+
+    if role == 'ADMIN':
+
+        role_admin = Role.query.filter_by(name=RoleEnum.ADMIN).first()
+        data.roles.append(role_admin)
+        db.session.commit()
 
     @require_role(role=required_role)
     def func_role():
@@ -99,7 +98,54 @@ def test_role_decorator(role, required_role):
         if func_role() != 'ok':
             assert False
     else:
-        if func_role() == 'ok':
+        if role == 'USER':
+            if func_role() == 'ok':
+                assert False
+        else:
+            if func_role() != 'ok':
+                assert False
+
+
+@pytest.mark.parametrize('role', ['USER', 'ADMIN'])
+@pytest.mark.parametrize('required_role', ['USER', 'ADMIN'])
+@pytest.mark.parametrize('redirect_page', ['library.index'])
+def test_role_decorator_custom_redirect(role, required_role, redirect_page):
+    email = g.person.email()
+    new_user = User(
+        email=email,
+        first_name=g.person.name(),
+        surname=g.person.surname(),
+        password_hash=g.cryptographic.hash(),
+        active=g.development.boolean()
+    )
+    db.session.add(new_user)
+    db.session.commit()
+
+    data = User.query.filter_by(email=email).first()
+
+    session['logged_in'] = True
+    session['id'] = data.id
+    session['email'] = data.email
+
+    if role == 'ADMIN':
+
+        role_admin = Role.query.filter_by(name=RoleEnum.ADMIN).first()
+        data.roles.append(role_admin)
+        db.session.commit()
+
+    @require_role(role=required_role, redirect_page=redirect_page)
+    def func_role():
+        return 'ok'
+
+    print(func_role(), data.roles)
+
+    if role == required_role:
+        if func_role() != 'ok':
             assert False
-
-
+    else:
+        if role == 'USER':
+            if func_role() == 'ok':
+                assert False
+        else:
+            if func_role() != 'ok':
+                assert False
