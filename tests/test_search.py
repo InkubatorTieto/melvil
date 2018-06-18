0d161db84c11bdb555cb8176181bbf5a35da6241
wojwarych <woj.warych@gmail.com>
2018-06-18 13:40:28 +0200

    added test for role of user

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,7 +1,16 @@
 from flask import url_for
 
+from models.users import RoleEnum, Role
 
-def test_search(session, client):
-    #
+def test_search(client, app_session):
+
     resp = client.get(url_for('library.search'))
     assert resp.status_code == 200
+
+
+def test_has_role(db_user):
+
+    role_admin = Role.query.filter_by(name=RoleEnum.ADMIN).first()
+    db_user.roles.append(role_admin)
+    assert db_user.has_role('ADMIN'), \
+        "has_role() method on User model does not work properly"
