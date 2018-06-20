12e961a77c926adc0d6952a503253fce875bdb62
oskam <oska.magdalena@gmail.com>
2018-06-20 14:51:06 +0200

    test update

--- tests/test_wishlist.py
+++ tests/test_wishlist.py
@@ -39,7 +39,7 @@ def test_add_like_function(db_user, db_wishlist_item):
 
 
 def test_wish_delete(db_wishlist_item):
-    db_wishlist_item.deleteWish(db_wishlist_item.id)
+    db_wishlist_item.delete_wish(db_wishlist_item.id)
     assert WishListItem.query.filter_by(
         id=db_wishlist_item.id)\
         .scalar() is None, "Wish delete failed"
