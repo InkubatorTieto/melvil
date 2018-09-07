c007380b93a6b935a03e09c1930a0ee6ba2565ab
wojwarych <woj.warych@gmail.com>
2018-09-07 14:02:04 +0200

    added bash script to removing old containers for fixing naming conflicts

---
+++ docker/remove-container.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+# clear *.pyc files
+. docker/clear-cache.sh
+
+remove_container () {
+  # cases for removing specific container
+  case "$1" in 
+   "t")
+      # find IDs of all test containers
+      found_containers=$(docker ps -a --filter 'name=melvil_tests' --format "{{.ID}}")
+      if [ '$found_containers' ] ; then
+        # remove test containers
+        remove_containers=$(docker rm $found_containers)
+      fi;;
+
+    "p")
+      case "$2" in
+        "m")
+            # find IDs of all migration containers
+            found_containers=$(docker ps -a --filter 'name=melvil_db_migration_prod' --format "{{.ID}}")
+            if [ '$found_containers' ] ; then
+              # remove migration containers
+              remove_containers=$(docker rm $found_containers)
+            fi;;
+        "u")
+            # find IDs of all migration containers
+            found_containers=$(docker ps -a --filter 'name=melvil_db_upgrade_prod' --format "{{.ID}}")
+            if [ '$found_containers' ] ; then
+              # remove migration containers
+              remove_containers=$(docker rm $found_containers)
+            fi;;
+        "d")
+            # find IDs of all migration containers
+            found_containers=$(docker ps -a --filter 'name=melvil_db_prod' --format "{{.ID}}")
+            if [ '$found_containers' ] ; then
+              # remove migration containers
+              remove_containers=$(docker rm $found_containers)
+            fi;;
+        "x")
+            # find IDs of all migration containers
+            found_containers=$(docker ps -a --filter 'name=upload_lib_items_prod' --format "{{.ID}}")
+            if [ '$found_containers' ] ; then
+              # remove migration containers
+              remove_containers=$(docker rm $found_containers)
+            fi;;
+      esac;;
+
+    "b")
+      case "$2" in
+        "m")
+            # find IDs of all migration containers
+            found_containers=$(docker ps -a --filter 'name=melvil_db_migration_dev' --format "{{.ID}}")
+            if [ '$found_containers' ] ; then
+              # remove migration containers
+              remove_containers=$(docker rm $found_containers)
+            fi;;
+        "u")
+            # find IDs of all migration containers
+            found_containers=$(docker ps -a --filter 'name=melvil_db_upgrade_dev' --format "{{.ID}}")
+            if [ '$found_containers' ] ; then
+              # remove migration containers
+              remove_containers=$(docker rm $found_containers)
+            fi;;
+        "d")
+            # find IDs of all migration containers
+            found_containers=$(docker ps -a --filter 'name=melvil_db_dev' --format "{{.ID}}")
+            if [ '$found_containers' ] ; then
+              # remove migration containers
+              remove_containers=$(docker rm $found_containers)
+            fi;;
+        "x")
+            # find IDs of all migration containers
+            found_containers=$(docker ps -a --filter 'name=upload_lib_items_dev' --format "{{.ID}}")
+            if [ '$found_containers' ] ; then
+              # remove migration containers
+              remove_containers=$(docker rm $found_containers)
+            fi;;
+      esac;;
+  esac
+}
--- run-server.sh
+++ run-server.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 
+# remove exisitng test containers
+. docker/remove-container.sh
+
 # clear *.pyc files
 . docker/clear-cache.sh
+
 
 DEV_DOCKER=./docker/docker-compose-dev.yml
 PROD_DOCKER=./docker/docker-compose.yml
 
 if [ "$1" == "tests" ] ; then
-
-  # check existence of previous test containers
-  test_cont=$(docker ps -a --filter 'name=melvil_tests' --format "{{.ID}}")
-  if [ "$test_cont" ] ; then
-    # remove exisitng test containers
-    . docker/docker-clear-tests.sh
-    # run tests
-  fi
+  #remove tests
+  remove_container t
   # run tests
   docker-compose -f $DEV_DOCKER run --name melvil_tests web pytest
 
 elif [ "$1" == "-p" ] ; then
     # production server
     if [ "$2" == "migrate" ] ; then
+      remove_container p m
       docker-compose -f $PROD_DOCKER run --name melvil_db_migration_prod web flask db migrate
       docker-compose -f $PROD_DOCKER stop postgresql
     elif [ "$2" == "upgrade" ] ; then
+      remove_container p u
       docker-compose -f $PROD_DOCKER run --name melvil_db_upgrade_prod web flask db upgrade
       docker-compose -f $PROD_DOCKER stop postgresql
    elif [ "$2" == "create-db" ] ; then
       # create initial database and set alembic's head
-      docker-compose -f $PROD_DOCKER run --name melvil_db_prod python create_db.py
+      remove_container p d
+      docker-compose -f $PROD_DOCKER run --name melvil_db_prod web python create_db.py
       docker-compose -f $PROD_DOCKER stop postgresql
-   elif [ "$1" == "load-xls" ] ; then
+   elif [ "$2" == "load-xls" ] ; then
       # load xls data into db
+      remove_container p x
       docker-compose -f $PROD_DOCKER run --name upload_lib_items_prod web flask load_xls_into_db
       docker-compose -f $PROD_DOCKER stop postgresql
    elif [ "$2" == "-b" ] ; then
@@ -44,17 +46,21 @@ elif [ "$1" == "-p" ] ; then
 else
    # development server
     if [ "$1" == "migrate" ] ; then
+      remove_container b m
       docker-compose -f $DEV_DOCKER run --name melvil_db_migration_dev web flask db migrate
       docker-compose -f $DEV_DOCKER stop postgresql
     elif [ "$1" == "upgrade" ] ; then
+      remove_container b u
       docker-compose -f $DEV_DOCKER run --name melvil_db_upgrade_dev web flask db upgrade
       docker-compose -f $DEV_DOCKER stop postgresql
    elif [ "$1" == "create-db" ] ; then
       # create initial database and set alembic's head
+      remove_container b d
       docker-compose -f $DEV_DOCKER run --name melvil_db_dev web python create_db.py
       docker-compose -f $DEV_DOCKER stop postgresql
    elif [ "$1" == "load-xls" ] ; then
       # load xls data into db
+      remove_container b x
       docker-compose -f $DEV_DOCKER run --name upload_lib_items_dev web flask load_xls_into_db
       docker-compose -f $DEV_DOCKER stop postgresql
    elif [ "$1" == "-b" ] ; then
