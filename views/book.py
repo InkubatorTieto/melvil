50d423efd2bd3108474a7418713443a07cb79491
Ochowicz Marta <marta.ochowicz@tieto.com>
2018-09-10 11:53:31 +0200

    MixedForm replaced by two separate forms for Book and Magazine

--- views/book.py
+++ views/book.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from flask import Blueprint
 from flask import render_template, request, session, abort
 
-from forms.book import BookForm, MixedForm, MagazineForm
+from forms.book import BookForm, MagazineForm, AddNewItemBookForm, AddNewItemMagazineForm
 from init_db import db
 from models import User, Tag, Magazine, Book, Author, LibraryItem
 
@@ -13,6 +13,8 @@ library_books = Blueprint('library_books', __name__,
 
 @library_books.route('/add_book', methods=['GET', 'POST'])
 def add_book():
+    book_form = AddNewItemBookForm(radio='book')
+    magazine_form = AddNewItemMagazineForm(radio='magazine')
     if request.method == 'GET':
         if 'logged_in' not in session:
             message_body = 'You are not logged in.'
@@ -20,129 +22,117 @@ def add_book():
             return render_template('message.html',
                                    message_title=message_title,
                                    message_body=message_body)
-        else:
-            form = MixedForm()
 
         return render_template('add_book.html',
-                               form=form,
-                               error=form.errors
+                               book_form=book_form,
+                               magazine_form=magazine_form,
+                               book_error=book_form.errors,
+                               magazine_error=magazine_form.errors
                                )
     else:
-        form = MixedForm()
-        if form.radio.data == 'book':
-            del form.issue
-            del form.title_of_magazine
-            if form.validate_on_submit():
-                tmp_authors = [[form.first_name.data, form.surname.data],
-                               [form.first_name_1.data, form.surname_1.data],
-                               [form.first_name_2.data, form.surname_2.data],
-                               ]
+        if book_form.submit1.data and book_form.validate():
+            tmp_authors = [[book_form.first_name.data, book_form.surname.data],
+                            [book_form.first_name_1.data, book_form.surname_1.data],
+                            [book_form.first_name_2.data, book_form.surname_2.data],
+                            ]
 
-                new_authors = []
+            new_authors = []
 
-                for first_name, surname in tmp_authors:
-                    if first_name is not '' and surname is not '':
-                        author = Author.query.filter_by(first_name=first_name,
+            for first_name, surname in tmp_authors:
+                if first_name is not '' and surname is not '':
+                    author = Author.query.filter_by(first_name=first_name,
                                                         last_name=surname
                                                         ).first()
-                        if not author:
-                            new_author = Author(
-                                first_name=first_name,
-                                last_name=surname
-                            )
-                            new_authors.append(new_author)
-                            db.session.add(new_author)
-                            db.session.commit()
-                        else:
-                            new_authors.append(author)
+                    if not author:
+                        new_author = Author(
+                            first_name=first_name,
+                            last_name=surname
+                        )
+                        new_authors.append(new_author)
+                        db.session.add(new_author)
+                        db.session.commit()
+                    else:
+                        new_authors.append(author)
 
-                tmp_tag = Tag.query.filter_by(name=form.tag.data).first()
-                if not tmp_tag:
-                    new_tag = Tag(
-                        name=form.tag.data
-                    )
-                    db.session.add(new_tag)
-                    db.session.commit()
-                else:
-                    new_tag = tmp_tag
-
-                new_book = Book(
-                    title=form.title.data,
-                    table_of_contents=form.table_of_contents.data,
-                    language=form.language.data,
-                    category=form.category.data,
-                    tags=[new_tag],
-                    description=form.description.data,
-                    isbn=form.isbn.data,
-                    authors=new_authors,
-                    original_title=form.original_title.data,
-                    publisher=form.publisher.data,
-                    pub_date=datetime(year=int(form.pub_date.data),
-                                      month=1,
-                                      day=1))
-                if book_exists(new_book):
-                    message_body = 'This book already exists.'
-                    message_title = 'Oops!'
-                    return render_template('message.html',
-                                           message_title=message_title,
-                                           message_body=message_body)
-                db.session.add(new_book)
+            tmp_tag = Tag.query.filter_by(name=book_form.tag.data).first()
+            if not tmp_tag:
+                new_tag = Tag(
+                    name=book_form.tag.data
+                )
+                db.session.add(new_tag)
                 db.session.commit()
+            else:
+                new_tag = tmp_tag
 
-                message_body = 'The book has been added.'
-                message_title = 'Success!'
+            new_book = Book(
+                title=book_form.title.data,
+                table_of_contents=book_form.table_of_contents.data,
+                language=book_form.language.data,
+                category=book_form.category.data,
+                tags=[new_tag],
+                description=book_form.description.data,
+                isbn=book_form.isbn.data,
+                authors=new_authors,
+                original_title=book_form.original_title.data,
+                publisher=book_form.publisher.data,
+                pub_date=datetime(year=int(book_form.pub_date.data),
+                                    month=1,
+                                    day=1))
+            if book_exists(new_book):
+                message_body = 'This book already exists.'
+                message_title = 'Oops!'
                 return render_template('message.html',
+                                        message_title=message_title,
+                                        message_body=message_body)
+            db.session.add(new_book)
+            db.session.commit()
+
+            message_body = 'The book has been added.'
+            message_title = 'Success!'
+            return render_template('message.html',
+                                    message_title=message_title,
+                                       message_body=message_body)
+
+
+        if magazine_form.submit2.data and magazine_form.validate():
+            tmp_tag = Tag.query.filter_by(name=magazine_form.tag.data).first()
+            if not tmp_tag:
+                new_tag = Tag(
+                    name=magazine_form.tag.data
+                )
+                db.session.add(new_tag)
+                db.session.commit()
+            else:
+                new_tag = tmp_tag
+
+            new_magazine = Magazine(
+                title=magazine_form.title_of_magazine.data,
+                table_of_contents=magazine_form.table_of_contents.data,
+                language=magazine_form.language.data,
+                category=magazine_form.category.data,
+                tags=[new_tag],
+                description=magazine_form.description.data,
+                year=datetime(year=int(magazine_form.pub_date.data),
+                                month=1,
+                                day=1),
+                issue=magazine_form.issue.data)
+
+            db.session.add(new_magazine)
+            db.session.commit()
+
+            message_body = 'The magazine has been added.'
+            message_title = 'Success!'
+            return render_template('message.html',
                                        message_title=message_title,
                                        message_body=message_body)
 
-            return render_template('add_book.html',
-                                   form=form,
-                                   error=form.errors)
 
-        if form.radio.data == 'magazine':
-            del form.publisher
-            del form.original_title
-            del form.isbn
-            del form.title
-            del form.first_name
-            del form.surname
-            if form.validate_on_submit():
-
-                tmp_tag = Tag.query.filter_by(name=form.tag.data).first()
-                if not tmp_tag:
-                    new_tag = Tag(
-                        name=form.tag.data
-                    )
-                    db.session.add(new_tag)
-                    db.session.commit()
-                else:
-                    new_tag = tmp_tag
-
-                new_magazine = Magazine(
-                    title=form.title_of_magazine.data,
-                    table_of_contents=form.table_of_contents.data,
-                    language=form.language.data,
-                    category=form.category.data,
-                    tags=[new_tag],
-                    description=form.description.data,
-                    year=datetime(year=int(form.pub_date.data),
-                                  month=1,
-                                  day=1),
-                    issue=form.issue.data)
-
-                db.session.add(new_magazine)
-                db.session.commit()
-
-                message_body = 'The magazine has been added.'
-                message_title = 'Success!'
-                return render_template('message.html',
-                                       message_title=message_title,
-                                       message_body=message_body)
-
-            return render_template('add_book.html',
-                                   form=form,
-                                   error=form.errors)
-
+        return render_template('add_book.html',
+                               book_form=book_form,
+                               magazine_form=magazine_form,
+                               book_error=book_form.errors,
+                               magazine_error=magazine_form.errors
+                               )
 
 @library_books.route('/edit_book/<int:item_id>', methods=['GET', 'POST'])
 def edit_book(item_id):
