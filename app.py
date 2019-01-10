32e21f82777f084e9543f63ce2c3e9da2a873ec0
jahenstein <lu.jaszewski@gmail.com>
2019-01-10 15:50:05 +0100

    login via ldap works, changes in db needed to make more complex operations - borrow book etc.

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from sqlalchemy.exc import OperationalError, TimeoutError
 
 from config import DevConfig, ProdConfig
 from init_db import db
-from ldap_utils import register_hooks, ldap_client
+from ldap_utils.ldap_utils import register_hooks, ldap_client
 from utils.xlsx_reader import get_books, get_magazines
 from utils.create_admin_user import create_super_user
 from views.book import library_books
