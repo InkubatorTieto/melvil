976d0b43d36edb95790b2f7a00fa6e4f16fef033
jahenstein <jahenstein@gmail.com>
2019-02-01 11:56:12 +0100

    Login via ldap works like a charm

--- models/users.py
+++ models/users.py
@@ -30,6 +30,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(128), unique=True, nullable=False)
     first_name = db.Column(db.String(64), nullable=False)
     surname = db.Column(db.String(64), nullable=False)
+    employee_id = db.Column(db.String(64), nullable=False)
     active = db.Column(db.Boolean)
     roles = db.relationship(
         "Role",
