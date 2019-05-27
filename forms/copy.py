4244db95e107ce0db9b41613e4dbb493f8a80ab7
jahenstein <jahenstein@gmail.com>
2019-05-27 10:25:42 +0200

    asset code is accepted in two formats, ab123456 or 123456

--- forms/copy.py
+++ forms/copy.py
@@ -6,13 +6,14 @@ from flask_wtf import FlaskForm
 
 from models import Copy
 
-""" Regex return True for assets like "wr123456".
+""" Regex return True for assets like "ab123456" or "123456".
 General template:
 two letters (letter case does not matter) + six digits
+or six digits only
 """
-asset_code_regex = Regexp('^[A-Z]{2}[0-9]{6}$',
+asset_code_regex = Regexp('^([A-Z]{2}[0-9]{6}|[0-9]{6})$',
                           flags=re.IGNORECASE,
-                          message='Insert valid asset code.')
+                          message='Insert valid asset code eg. ab123456 or 123456')
 
 
 class CopyAddForm(ModelForm, FlaskForm):
