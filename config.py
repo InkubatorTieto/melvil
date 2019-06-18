cc02641c039e5720d473398d0168b3c942ee71af
root <root@usv000110.eu.tieto.com>
2019-06-18 12:18:51 +0000

    small changes needed to run properly on production server

--- config.py
+++ config.py
@@ -47,4 +47,19 @@ class DevConfig(Config):
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = getenv("DATABASE_URL", "")
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+    SECRET_KEY = "4f\g45t45gfjerkfefker"
+    SECURITY_PASSWORD_SALT = "my_precious_two"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_POOL_TIMEOUT = 30
+    DEBUG = True
+    DB_ENGINE = getenv("DB_ENGINE")
+    DB_USER = getenv("DB_USER")
+    DB_PASSWORD = getenv("DB_PASSWORD")
+    DB_HOST = getenv("DB_HOST")
+    DB_PORT = getenv("DB_PORT")
+    DB_NAME = getenv("DB_NAME")
+
+    SQLALCHEMY_DATABASE_URI = "{0}://{1}:{2}@{3}:{4}/{5}".format(
+        DB_ENGINE, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
+    )
