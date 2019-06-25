fc42d09a0133ca8574211da646ca26d5af254b38
Rafal Skowronski <rafal.skowronski@gmail.com>
2019-06-25 10:53:42 +0200

    Fix for executing python script by cron

--- cron/src/app/data_access_layer.py
+++ cron/src/app/data_access_layer.py
@@ -4,37 +4,45 @@ from sqlalchemy import Table, Column, Integer, String, Boolean, \
 from sqlalchemy.engine import Connection
 from sqlalchemy_utils import ChoiceType
 
+
 class BookStatus(Enum):
     RESERVED = 1
     BORROWED = 2
     RETURNED = 3
 
+
 class DataAccessLayer:
     connection = None
     engine = None
-    connection_string = None
-    metadata = MetaData()
+    copy = None
+    rental_log = None
 
-    copy = Table('copy', metadata,
-        Column('id', Integer, primary_key=True),
-        Column('asset_code', String, unique=True),
-        Column('library_item_id', Integer),
-        Column('shelf', String),
-        Column('has_cd_disk', Boolean),
-        Column('available_status', ChoiceType(BookStatus, impl=Integer())))
+    def __init__(self, *args):
+        metadata = MetaData()
 
-    rental_log = Table('rental_log', metadata,
-        Column('id', Integer, primary_key=True),
-        Column('copy_id', Integer, ForeignKey("copy.id"), nullable=False),
-        Column('_borrow_time', DateTime),
-        Column('_return_time', DateTime),
-        Column('book_status', ChoiceType(BookStatus, impl=Integer())),
-        Column('_reservation_begin', DateTime),
-        Column('_reservation_end', DateTime))
+        self.copy = Table('copy',
+                          metadata,
+                          Column('id', Integer, primary_key=True),
+                          Column('asset_code', String, unique=True),
+                          Column('library_item_id', Integer),
+                          Column('shelf', String),
+                          Column('has_cd_disk', Boolean),
+                          Column('available_status',
+                                 ChoiceType(BookStatus, impl=Integer())))
 
-    def init(self, connection_string):
-        self.connection_string = connection_string
-        self.engine = create_engine(connection_string)
+        self.rental_log = Table('rental_log',
+                                metadata,
+                                Column('id', Integer, primary_key=True),
+                                Column('copy_id',
+                                       Integer,
+                                       ForeignKey("copy.id"),
+                                       nullable=False),
+                                Column('_borrow_time', DateTime),
+                                Column('_return_time', DateTime),
+                                Column('book_status',
+                                       ChoiceType(BookStatus, impl=Integer())),
+                                Column('_reservation_begin', DateTime),
+                                Column('_reservation_end', DateTime))
+
+        self.engine = create_engine(*args)
         self.connection = Connection(self.engine)
-
-data_access_layer = DataAccessLayer()
--- cron/src/app/update_reservation_status.py
+++ cron/src/app/update_reservation_status.py
@@ -1,43 +1,61 @@
 from os import getenv
 from datetime import datetime
-from sqlalchemy.sql import select, update
+from urllib.parse import quote_plus
+from sqlalchemy.sql import select
+from sqlalchemy.engine.url import URL
 from dotenv import load_dotenv
 
-from data_access_layer import data_access_layer, BookStatus
+from data_access_layer import DataAccessLayer, BookStatus
 
-def get_connection_string():
-    load_dotenv()
-    DB_ENGINE = getenv("DB_ENGINE")
-    DB_USER = getenv("DB_USER")
-    DB_PASSWORD = getenv("DB_PASSWORD")
-    DB_HOST = getenv("DB_HOST")
-    DB_PORT = getenv("DB_PORT")
-    DB_NAME = getenv("DB_NAME")
 
-    return "{0}://{1}:{2}@{3}:{4}/{5}".format(
-        DB_ENGINE, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
-    )
+def get_url():
+    return URL(
+        drivername=getenv("DB_ENGINE"),
+        username=getenv("DB_USER"),
+        password=quote_plus(getenv("DB_PASSWORD")),
+        host=getenv("DB_HOST"),
+        port=getenv("DB_PORT"),
+        database=getenv("DB_NAME"))
+
 
 def check_reservation_status_db(dal):
-    connection = dal.connection.execution_options(isolation_level="REPEATABLE READ")
+    connection = dal.connection.execution_options(
+        isolation_level="REPEATABLE READ")
     rental_log = dal.rental_log
     copy = dal.copy
 
     with connection.begin():
         items = connection.execute(
-            select([copy.c.id, rental_log.c.id, rental_log.c._reservation_end]).select_from(
-                copy.join(rental_log)).where(rental_log.c.book_status == BookStatus.RESERVED
-            )
+            select([
+                copy.c.id,
+                copy.c.library_item_id,
+                rental_log.c.id,
+                rental_log.c._reservation_end
+                ])
+            .select_from(copy.join(rental_log))
+            .where(rental_log.c.book_status == BookStatus.RESERVED)
         ).fetchall()
-        for copy_id, renatl_log_id, reservation_end in items:
-            if reservation_end <= datetime.now():
+        for copy_id, library_item_id, renatl_log_id, reservation_end in items:
+            if True:  # reservation_end <= datetime.now():
                 # pylint: disable=no-value-for-parameter
-                connection.execute(rental_log.update().where(rental_log.c.id == renatl_log_id).values(book_status=BookStatus.RETURNED))
-                connection.execute(copy.update().where(copy.c.id == copy_id).values(available_status=BookStatus.RETURNED))
+                connection.execute(
+                    rental_log
+                    .update()
+                    .where(rental_log.c.id == renatl_log_id)
+                    .values(book_status=BookStatus.RETURNED)
+                )
+                connection.execute(
+                    copy
+                    .update()
+                    .where(copy.c.id == copy_id)
+                    .values(available_status=BookStatus.RETURNED)
+                )
                 # pylint: enable=no-value-for-parameter
+                print("[{}] Cancelled reservation for library item id: {}"
+                      .format(datetime.now(), library_item_id))
+
 
 if __name__ == '__main__':
-    print("---")
-    connection_string = get_connection_string()
-    data_access_layer.init(connection_string)
+    load_dotenv()
+    data_access_layer = DataAccessLayer(get_url())
     check_reservation_status_db(data_access_layer)
