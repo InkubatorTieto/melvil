62c47e3ac9daa660bf1ee5c42c295b7d6bc70f8d
Rafal Skowronski <rafal.skowronski@gmail.com>
2019-08-13 12:15:41 +0200

    Move flask-mail options to environment variables

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ class Config(object):
     DATABASE_URI = ""
 
     # email server
-    MAIL_SERVER = "smtp.gmail.com"
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
+    MAIL_SERVER = getenv("MAIL_SERVER")
+    MAIL_PORT = getenv("MAIL_PORT")
+    MAIL_USE_TLS = getenv("MAIL_USE_TLS") == "True"
+    MAIL_USE_SSL = getenv("MAIL_USE_SSL") == "True"
     MAIL_USERNAME = getenv("MAIL_USERNAME")
     MAIL_PASSWORD = getenv("MAIL_PASSWORD")
     ADMINS = [getenv("MAIL_USERNAME")]
