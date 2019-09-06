c2ec4f098977582218bc36e5a0569895bd14a926
jahenstein <jahenstein@gmail.com>
2019-09-06 13:55:04 +0200

    stickler fix

--- tests/test_reservation.py
+++ tests/test_reservation.py
@@ -3,17 +3,17 @@ from flask import url_for
 
 def test_status_code(client, app_session, db_copies):
     resp = client.get(url_for(
-            "library.reserve",
-            item_id=0,
-            copy_id=db_copies[0].id
+        "library.reserve",
+        item_id=0,
+        copy_id=db_copies[0].id
     ))
     assert resp.status_code == 302
 
     for copy in db_copies[4:]:
         resp = client.get(url_for(
-                "library.reserve",
-                item_id=0,
-                copy_id=copy.id
+            "library.reserve",
+            item_id=0,
+            copy_id=copy.id
         ))
         assert resp.status_code == 409
 
